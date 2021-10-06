daede4d31158ba7790053af7e1be032641ab3da6
protofrancesco <79990211+protofrancesco@users.noreply.github.com>
2021-10-06 17:37:34 +0200

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -32,18 +32,7 @@ module.exports = {
         }
       }
     }
-    {
-      use: "@gridsome/source-filesystem",
-      options: {
-        path: "servizi/**/*.md",
-        typeName: "JournalPost",
-        resolveAbsolutePaths: true,
-        remark: {
-          externalLinksTarget: "_blank",
-          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
-        }
-      }
-    }
+
   ],
   transformers: {
     remark: {
